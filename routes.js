f5c61a6f0bbc34c2ed7bad28bdcd5fd26546c2a7
kingdido999 <kingdido999@gmail.com>
2015-04-08 22:47:11 -0400

    minor change

--- routes.js
+++ routes.js
@@ -10,24 +10,21 @@ var photo = require('./handlers/photo');
 module.exports = function(app, passport) {
 
 	// =====================================
+	// AUTHENTICATION ======================
+	// =====================================
+	app.get('/login', auth.getLogin);
+	app.post('/login', auth.postLogin);
+	app.get('/signup', auth.getSignup);
+	app.post('/signup', auth.postSignup);
+	app.get('/logout', auth.logout);
+
+	// =====================================
 	// HOME PAGE ===========================
 	// =====================================
 	app.get('/', routes.index);
 	app.get('/albums', routes.album);
 	app.get('/tags', routes.tags);
 	app.post('/showAllTaggedPhotos', routes.showAllTaggedPhotos);
-
-	// =====================================
-	// LOGIN ===============================
-	// =====================================
-	app.get('/login', auth.getLogin);
-	app.post('/login', auth.postLogin);
-
-	// =====================================
-	// SIGNUP ==============================
-	// =====================================
-	app.get('/signup', auth.getSignup);
-	app.post('/signup', auth.postSignup);
 
 	// =====================================
 	// PROFILE =============================
@@ -39,11 +36,6 @@ module.exports = function(app, passport) {
 	app.post('/deletePhoto', isLoggedIn, profile.deletePhoto);
 	app.post('/addTags', isLoggedIn, profile.addTags);
 	app.post('/showUserTaggedPhotos', isLoggedIn, profile.showUserTaggedPhotos);
-
-	// =====================================
-	// LOGOUT ==============================
-	// =====================================
-	app.get('/logout', auth.logout);
 
 	// =====================================
 	// UPLOAD ==============================

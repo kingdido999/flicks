d483e2fb92dec332e3d9c73799c14384d6609afa
kingdido999 <kingdido999@gmail.com>
2015-04-09 14:27:16 -0400

    finished adding comments

--- handlers/photo.js
+++ handlers/photo.js
@@ -11,15 +11,13 @@ module.exports = {
     var canComment = true;
 
 		var query = "\
-			SELECT owner_id, caption, path, date_of_creation \
+			SELECT id, owner_id, caption, path, date_of_creation \
 			FROM Photo P \
 			WHERE id = ?";
 		var params = [photo_id];
 
 		connection.query(query, params, function(err, rows) {
 			if (err) throw err;
-
-      console.log(req.session.user);
 
       // check if user has signed in
       if (typeof req.session.user != 'undefined') {
@@ -41,17 +39,35 @@ module.exports = {
 
   addComment: function(req, res) {
 		var addComment = true;
+    var author_id = -1;
+    var author_email = 'Visitor';
+
+    // check if user has signed in
+    if (typeof req.session.user != 'undefined') {
+      author_id = req.session.user.id;
+      author_email = req.session.user.email;
+    }
+
+    var photo_id = req.body.photo_id;
 		var comment = req.body.comment;
-		// var owner_id =
 
 		// check if comment is empty
-		// if (comment.length > 0) {
-		// 	var query = "\
-		// 		INSERT INTO Comment (owner_id, photo_id, text) VALUES (?, ?, ?);\
-		// 		UPDATE User SET num_comments = num_comments + 1 WHERE id = ?;\
-		// 		UPDATE Photo SET num_comments = num_comments + 1 WHERE id = ?";
-		// 	var params = [req.]
-		// }
+		if (comment.length > 0) {
+			var query = "\
+				INSERT INTO Comment (owner_id, photo_id, text) VALUES (?, ?, ?);\
+				UPDATE User SET num_comments = num_comments + 1 WHERE id = ?;\
+				UPDATE Photo SET num_comments = num_comments + 1 WHERE id = ?";
+			var params = [author_id, photo_id, comment, author_id, photo_id];
+
+      console.log(author_id, author_email, photo_id, comment);
+      connection.query(query, params, function(err, rows) {
+        if (err) throw err;
+
+        console.log('Comment added!');
+
+        res.redirect('/photo?id=' + photo_id);
+      });
+		}
 
 	},
 }
